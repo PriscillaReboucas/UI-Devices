f5d80d177e5a35f4ed1347fdb26f9280d04d6ec9
Priscilla Rebouças <97620696+PriscillaReboucas@users.noreply.github.com>
2022-12-09 15:39:27 +0100

    added grid icon

--- src/components/GridView.tsx
+++ src/components/GridView.tsx
@@ -2,16 +2,14 @@ import './GridView.css';
 import { useContext } from 'react';
 import { DevicesContext } from '../DevicesContext';
 import { DevicesContextType } from '../@types/devices.d';
+import union from '../assets/Union.png'
 
 export const GridView = () => {
   const { setView } = useContext(DevicesContext) as DevicesContextType;
 
     return (
         <div className='actions__gridView--container' onClick={() => setView('grid')}>
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
+            <img className='gridview__image' src={union} alt="grid icon" />
         </div>
     )
-}
+}   
