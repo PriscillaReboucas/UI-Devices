f332ae060e20b49297cb4cb6ef27fa28ee43d36c
Priscilla Rebouças <97620696+PriscillaReboucas@users.noreply.github.com>
2022-12-10 02:25:39 +0100

    added filter style

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -4,11 +4,13 @@ import { DevicesContext } from '../DevicesContext';
 import { DevicesContextType } from '../@types/devices.d';
 
 export const Filter = () => {
-  const { setFilter } = useContext(DevicesContext) as DevicesContextType;
+  const { filter, setFilter } = useContext(DevicesContext) as DevicesContextType;
 
     return (
-        <select className='actions__filter' aria-label='Filter by product line' onChange={(e) => setFilter(e.target.value)}>
+        <select className='actions__filter' aria-label='Filter by product line'
+        onChange={(e) => setFilter(e.target.value)} value={filter}>
             <option value='all'>Filter</option>
+            <option className='filter__title' disabled>Product Line</option>
             <option value='UniFi'>UniFi</option>
             <option value='UniFi LTE'>UniFi LTE</option>
             <option value='UniFi Protect'>UniFi Protect</option>
