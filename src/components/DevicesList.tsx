2e0386f79a66d16961afcb8cc37c09b87ccb0a42
Priscilla Rebouças <97620696+PriscillaReboucas@users.noreply.github.com>
2022-12-10 01:11:56 +0100

    changing list css

--- src/components/DevicesList.tsx
+++ src/components/DevicesList.tsx
@@ -1,6 +1,6 @@
 import './DevicesList.css';
 import { IDevices } from '../@types/devices.d';
-import { DeviceLine } from './DeviceLine';
+import { Link } from 'react-router-dom';
 
 type Props = {
     devices: IDevices[];
@@ -9,19 +9,26 @@ type Props = {
 export const DevicesList = ({devices}:Props) => {
     return (
     <section className='devicesList--container'>
-        <section className='devicesList__titles--container'>
-            <h2 className='title title__small'>{devices.length} devices</h2>
-            <h2 className='title title__big'>Product Line</h2>
-            <h2 className='title title__big'>Name</h2>
-        </section>
-        <section className='devicesList__products--container'>
+        <article className='devicesList--wrapper'>
+            <p className='title title__description'>{devices.length} devices</p>
+            <h2 className='title title__line'>Product Line</h2>
+            <h2 className='title title__name'>Name</h2>
+        </article>
         {devices && devices.map((device: IDevices) => {
-            return (<article className='products--container' key={device.model_id}>
-                        <DeviceLine device={device} />
-                    </article>
+            return (
+        <Link className='devicesList--wrapper action'  
+        key={device.model_id}
+        to={`/devices/${device.product.name}`}
+        >
+            <div className='devicesList__image--container'>
+                <img className='devicesList__image' src={`https://static.ui.com/fingerprint/ui/icons/${device.icon.id}_25x25.png`} alt={device.product.name} />
+            </div>
+            <p className="title line__text--title">{device.line.name}</p>
+            <p className="title line__text--subtitle">{device.product.name}</p>
+        </Link>
             );
         })}
-        </section>
+
     </section>
     )
 }
